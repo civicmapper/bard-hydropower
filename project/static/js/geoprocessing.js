c84b1f49dda343196acfab02a3e786eeb3a13205
Christian Gass <christiangass@outlook.com>
2017-07-05 23:19:27 -0400

    make analysis functions more modular

--- project/static/js/geoprocessing.js
+++ project/static/js/geoprocessing.js
@@ -5,14 +5,15 @@
 /**
  * run the two GP tasks at once
  */
-var runGP = function () {
+var runGP = function (runWatershed, runProfile) {
   var e = $.Deferred();
   var w = $.Deferred();
-
+  runWatershed = true;
+  runProfile = true;
   /**
    * run the Elevation Profile service
+   * if runProfile is False, then skip GP, return null
    */
-  //function runElevProfileGP() {
     var elevProfileService = L.esri.GP.service({
       url: "http://elevation.arcgis.com/arcgis/rest/services/Tools/ElevationSync/GPServer/Profile",
       useCors: true,
@@ -49,12 +50,11 @@ var runGP = function () {
         }
       });
     });
-  //}
-
+  
   /**
    * run the Watershed service
+   * if runWatershed is False, then skip GP
    */
-  //function runWatershedGP () {
     var watershedService = L.esri.GP.service({
       url: "http://hydro.arcgis.com/arcgis/rest/services/Tools/Hydrology/GPServer/Watershed",
       useCors: true,
@@ -101,9 +101,7 @@ var runGP = function () {
           w.resolve(result);
         }
       });
-    
     });
-  //}
   
   /* when runGP runs, it first hits $.Deferred. The first argument is a function
    * that executes before anything else. That argument is a $.when function,
@@ -118,111 +116,36 @@ var runGP = function () {
     });
   });
   return x;
-
 };
 
-var calculatePower = function(head,area,envflow,efficiency) {
-  calcRequestURL = Flask.url_for("api", {
-    "area": area, "head": head, "envflow": envflow, "efficiency": efficiency
-  });
-  
-  $.get({
-    url: calcRequestURL,
-    success: function(data) {  
-      if (head < 0) {
-         $('#msg-status').html(makeAlert("The head calculation returned a negative value. Make sure the line was drawn downstream&rarr;upstream.", 'warning'));
-      } else {
-        //$('#msg-status').html(makeAlert(null,'success'));
-        $('#msg-status').hide();
-      }
-      
-      $('#msg-text').append('<h2><small>Power:</small>&nbsp;' + data.result.power + '&nbsp;<small>kW/year</small></h2>');
-      $('#msg-status-power').html(makeAlert("Power Generation Est.: Complete", 'success'));
-      $('#analyze-button-item').html(clearButton);
-      $('#msg-text').show();    
-    }
-  });
-}
-
-var analyzeGPResults = function(elevProfileResult,watershedResult) {
-  var msg = "Calculating Power Potential...";
-  console.log(msg);
-  $('#msg-status-power').html(makeAlert(msg, 'info'));
-  console.log(elevProfileResult);
-  console.log(watershedResult);
-  
-  /*post process the GP service results with these two methods:
-  hp.profile = elevProfileResult;
-  hp.calcHead();
-  hp.watershed = watershedResult;
-  hp.getArea();
-  hp.calcPower(null,null);
-  */
- 
-  // get the area value, which is buried in the service result object
-  var area = watershedResult.WatershedArea.features[0].properties.AreaSqKm;
-  $('#msg-text').append('<h3><small>Area:</small>&nbsp;' + _round(area,2) + '&nbsp;<small>km^2</small></h3>');
-  
-  //get the line from the result object
-  var line = elevProfileResult.OutputProfile.features[0];
-  // get the coords from the line
-  var coords = line.geometry.coordinates;
-  // get the z values from the first and last coordinate
-  var firstZ = coords[0][2];
-  var lastZ = coords[coords.length - 1][2];
-  // save the difference
-  var head =  Math.abs(lastZ - firstZ);
-  
-
-  $('#msg-text').append('<h3><small>Head:</small>&nbsp;' + _round(head,2) + '&nbsp;<small>meters</small></h3>');
-
-  /*
-  // calculate power
-  var Qavail = (area * 1.6);
-  //where x is a range from 0.1 to 0.5 with default value of 0.3 (edited)
-  var x = 0.3;
-  var Qenv = (area * x);
-  var Quseable = Qavail - Qenv;
-  //Power in kW; where e is a variable with default value 0.7 (edited)
-  var e = 0.7;
-  var p = Quseable * head * (0.084) * e;
-  var power = _round(p, 2);
-  */
-  calculatePower(head,area,envflow,efficiency);
-  var envflow = 0, efficiency = 0;
-  calcRequestURL = Flask.url_for("api", {
-    "area": area, "head": head, "envflow": envflow, "efficiency": efficiency
-  });
-  
-  $.get({
-    url: calcRequestURL,
-    success: function(data) {
-      if (head < 0) {
-         $('#msg-status').html(makeAlert("The head calculation returned a negative value. Make sure the line was drawn downstream&rarr;upstream.", 'warning'));
-      } else {
-        //$('#msg-status').html(makeAlert(null,'success'));
-        $('#msg-status').hide();
-      }
-      
-      $('#msg-text').append('<h2><small>Power:</small>&nbsp;' + data.result.power + '&nbsp;<small>kW/year</small></h2>');
-      $('#msg-status-power').html(makeAlert("Power Generation Est.: Complete", 'success'));
-      $('#analyze-button-item').html(clearButton);
-      $('#msg-text').show();    
-    }
-  });
-  
-  /*
-   //check result. It must be a positive number
-  if (head < 0) {
-     $('#msg-status').html(makeAlert("The head calculation returned a negative value. Make sure the line was drawn downstream&rarr;upstream.", 'warning'));
-  } else {
-    //$('#msg-status').html(makeAlert(null,'success'));
-    $('#msg-status').hide();
-  }
-  
-  $('#msg-text').append('<h2><small>Power:</small>&nbsp;' + power + '&nbsp;<small>kW/year</small></h2>');
+var analyzeSuccess = function() {
+  $('#msg-status').hide();
+  $('#msg-text').append('<h2><small>Power:</small>&nbsp;' + hp.power + '&nbsp;<small>kW/year</small></h2>');
+  // status, button, and message visibilty
   $('#msg-status-power').html(makeAlert("Power Generation Est.: Complete", 'success'));
   $('#analyze-button-item').html(clearButton);
   $('#msg-text').show();
-  */
+}
+
+var analyzeGPResults = function(elevProfileResult,watershedResult) {
+  // display status
+  var msg = "Calculating Power Potential...";
+  console.log(msg);
+  $('#msg-status-power').html(makeAlert(msg, 'info'));
+  // calculate head
+  if (elevProfileResult) {
+    console.log(elevProfileResult);
+    hp.profile = elevProfileResult;
+    hp_calcHead();
+  }
+  $('#msg-text').append('<h3><small>Head:</small>&nbsp;' + _round(hp.head,2) + '&nbsp;<small>meters</small></h3>');
+  // calculate area
+  if (watershedResult) {
+    console.log(watershedResult);
+    hp.watershed = watershedResult;
+    hp_getArea();
+  }
+  $('#msg-text').append('<h3><small>Area:</small>&nbsp;' + _round(hp.area, 2) + '&nbsp;<small>km^2</small></h3>');
+  // calculate power
+  hp_calculatePower();
 };
