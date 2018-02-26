86c8b9aa5fe14fd42c1f3725150753fbe4e16ac8
Christian Gass <christiangass@outlook.com>
2018-02-26 17:01:30 -0500

    added geocoder/place search

--- project/static/js/map.js
+++ project/static/js/map.js
@@ -7,51 +7,75 @@
 /*******************************************************************************
  * create map with basemap and supplemental layers
  */
-var map = L.map('map', {
-        maxZoom: 22
+var map = L.map("map", {
+    maxZoom: 22
+}).setView([42.0170202, -73.9144284], 12);
+
+var layer_streams = L.esri
+    .featureLayer({
+        url: "https://services.arcgis.com/vT1c5Cjxbz2EbiZw/arcgis/rest/services/BardMicroHydro/FeatureServer/7"
     })
-    .setView([42.0170202, -73.9144284], 18);
+    .addTo(map);
 
-var layer_streams = L.esri.featureLayer({
-    url: "https://services.arcgis.com/vT1c5Cjxbz2EbiZw/arcgis/rest/services/BardMicroHydro/FeatureServer/7",
-}).addTo(map);
-
-var layer_dams = L.esri.featureLayer({
-    url: "https://services.arcgis.com/vT1c5Cjxbz2EbiZw/arcgis/rest/services/BardMicroHydro/FeatureServer/1",
-}).addTo(map);
+var layer_dams = L.esri
+    .featureLayer({
+        url: "https://services.arcgis.com/vT1c5Cjxbz2EbiZw/arcgis/rest/services/BardMicroHydro/FeatureServer/1"
+    })
+    .addTo(map);
 
 /**
  * add a vector map overlay for labeling (vector tiles are not supported
  * with L.esri.webmap natively, so we do it here instead)
  */
-L.esri.Vector.basemap('Hybrid').addTo(map);
-//*/
-
-L.esri.basemapLayer('Imagery').addTo(map);
-
-//var webmap = L.esri.webMap('c7bad780627d4175abc7174278f69308', {map: map});
+L.esri.basemapLayer("Imagery").addTo(map);
+L.esri.Vector.basemap("Hybrid").addTo(map);
 
 /**
- * do some things with the layers loads into the webmap object when it's loaded.
- *
-webmap.on('load', function() {
-   // store a list of layers from the webmap
-    var overlayMaps = {};
-    
-    // iterate over layers the webmap
-    webmap.layers.map(function(layer) {
-      //store a list of layers from the webmap
-      overlayMaps[layer.title] = layer.layer;
-      
-      if (layer.type == "TML") {
-        layer.layer.options.maxZoom = 22;
-        layer.layer.options.maxNativeZoom = 18;          
-        }
-    });
-    L.control.layers({}, overlayMaps, {
-        position: 'bottomleft'
-    }).addTo(webmap._map);
+ * create the geocoder/search widget and supporting L.layerGroup
+ */
+var searchControl = L.esri.Geocoding.geosearch({ position: "topright" }).addTo(
+    map
+);
+
+var results = L.layerGroup().addTo(map);
+
+searchControl.on("results", function(data) {
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+        var result = L.circleMarker(data.results[i].latlng).bindPopup(
+            "<p>" + data.results[i].text + "</p>"
+        );
+        results.addLayer(result);
+        result.openPopup();
+    }
 });
 
-console.log(map.getPanes());
-//*/
+map.on("click", function(e) {
+    results.clearLayers();
+});
+
+/**
+ * build the map from an AGOL web map, then do some things with the layers
+ * loads into the webmap object when it's loaded.
+ */
+//var webmap = L.esri.webMap('c7bad780627d4175abc7174278f69308', {map: map});
+// webmap.on('load', function() {
+//    // store a list of layers from the webmap
+//     var overlayMaps = {};
+
+//     // iterate over layers the webmap
+//     webmap.layers.map(function(layer) {
+//       //store a list of layers from the webmap
+//       overlayMaps[layer.title] = layer.layer;
+
+//       if (layer.type == "TML") {
+//         layer.layer.options.maxZoom = 22;
+//         layer.layer.options.maxNativeZoom = 18;
+//         }
+//     });
+//     L.control.layers({}, overlayMaps, {
+//         position: 'bottomleft'
+//     }).addTo(webmap._map);
+// });
+
+// console.log(map.getPanes());
