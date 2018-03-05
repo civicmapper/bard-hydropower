ac1b0513a27e58714d1c730db2e5d92ee7636dec
Christian Gass <christiangass@outlook.com>
2018-03-05 17:30:59 -0500

    clean up

--- project/static/js/map.js
+++ project/static/js/map.js
@@ -30,26 +30,36 @@ var layer_dams = L.geoJSON(null, {
         }
     }
 });
+/**
+ * ClusterGroup layer for Dams (handles visual clustering)
+ */
 var layer_dams_clusters = L.markerClusterGroup();
+/**
+ * asynchronous retrieval of dams data.
+ */
 $.getJSON("/static/assets/data/nysdams.geojson", function(data) {
     layer_dams.addData(data);
     layer_dams_clusters.addLayer(layer_dams);
 });
 
+/**
+ * National Hydrographic Dataset (this from Esri's AGOL premium service; requires token.)
+ */
 var layer_streams = L.esri.dynamicMapLayer({
     url: "https://landscape1.arcgis.com/arcgis/rest/services/USA_NHD_HighRes/MapServer",
     opacity: 0.9,
     token: tokens.arcgis
 });
-//     .bindPopup(function (err, fC, response) {
-//     var count = featureCollection.features.length;
-//     var template = "";
-//     if (count) {
-//         $.each(function (i, v) {
-//             template += "";
-//         })
-//     }
-// });
+
+/**
+ * hold geocoding results here
+ */
+var searchResults = L.layerGroup();
+
+/**
+ * watershed area polygon. place holder for watershed results.
+ */
+var watershedArea = L.featureGroup();
 
 /*******************************************************************************
  * configure basemaps (for custom L.basemap control)
@@ -83,14 +93,25 @@ var basemaps = [
 ];
 
 /*******************************************************************************
- * basemaps (for custom L.basemap control)
+ * map configuration
  */
-
 var map = L.map("map", {
     maxZoom: 22
-}).setView([42.921683, -76.2419582], 7);
+        // }).setView([42.921683, -76.2419582], 7);
+}).setView([42.0169164, -73.9141064], 18);
 
+/**
+ * add layers
+ */
+map.addLayer(layer_dams_clusters);
+map.addLayer(watershedArea);
+map.addLayer(searchResults);
+
+/**
+ * add controls
+ */
 map.addControl(
+    // custom basemap control
     L.control.basemaps({
         basemaps: basemaps,
         tileX: 0, // tile X coordinate
@@ -98,81 +119,31 @@ map.addControl(
         tileZ: 1 // tile zoom level
     })
 );
-
-L.control
+// layer control
+map.addControl(
+    L.control
     .layers({}, { "NYS Dams": layer_dams_clusters, "National Hydrography": layer_streams })
     .setPosition("bottomright")
-    .addTo(map);
+);
+// geocoding conrol
+map.addControl(
+    L.esri.Geocoding.geosearch({
+        position: "topright"
+    }).on("results", function(data) {
+        searchResults.clearLayers();
+        for (var i = data.results.length - 1; i >= 0; i--) {
+            var result = L.circleMarker(data.results[i].latlng).bindPopup(
+                "<p>" + data.results[i].text + "</p>"
+            );
+            searchResults.addLayer(result);
+            result.openPopup();
+        }
+    })
+);
 
-map.addLayer(layer_dams_clusters);
-
-/**
- * add a vector map overlay for labeling. Since vector tiles are not supported
- * with L.esri.webmap natively, and we're using the Leaflet-Basemaps plugin,
- * we need to add/remove this overlay via event listener
+/*******************************************************************************
+ * map listeners
  */
-
-// var imageryBasemapLabels = L.esri.Vector.basemap("Hybrid", {
-//     pane: "overlayPane"
-// });
-
-// map.on("baselayerchange", function(e) {
-//     // console.log(e);
-
-//     if (e.options.label == "Esri Imagery") {
-//         imageryBasemapLabels.addTo(map);
-//         console.log("imageryBasemapLabels pane:", imageryBasemapLabels.getPane());
-//     } else {
-//         imageryBasemapLabels.removeFrom(map);
-//     }
-// });
-
-/**
- * create the geocoder/search widget and supporting L.layerGroup
- */
-var searchControl = L.esri.Geocoding.geosearch({
-    position: "topright"
-}).addTo(map);
-
-var searchResults = L.layerGroup().addTo(map);
-
-searchControl.on("results", function(data) {
-    searchResults.clearLayers();
-    for (var i = data.results.length - 1; i >= 0; i--) {
-        var result = L.circleMarker(data.results[i].latlng).bindPopup(
-            "<p>" + data.results[i].text + "</p>"
-        );
-        searchResults.addLayer(result);
-        searchResults.openPopup();
-    }
-});
-
 map.on("click", function(e) {
     searchResults.clearLayers();
 });
-
-/**
- * build the map from an AGOL web map, then do some things with the layers
- * loads into the webmap object when it's loaded.
- */
-//var webmap = L.esri.webMap('c7bad780627d4175abc7174278f69308', {map: map});
-// webmap.on('load', function() {
-//    // store a list of layers from the webmap
-//     var overlayMaps = {};
-
-//     // iterate over layers the webmap
-//     webmap.layers.map(function(layer) {
-//       //store a list of layers from the webmap
-//       overlayMaps[layer.title] = layer.layer;
-
-//       if (layer.type == "TML") {
-//         layer.layer.options.maxZoom = 22;
-//         layer.layer.options.maxNativeZoom = 18;
-//         }
-//     });
-//     L.control.layers({}, overlayMaps, {
-//         position: 'bottomleft'
-//     }).addTo(webmap._map);
-// });
-
-// console.log(map.getPanes());
